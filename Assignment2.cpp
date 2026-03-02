857d5298f7e008bcd2e6c7143ad0ad0db0e2e7ff
LuisT <Gutytrejo1502@gmail.com>
2026-03-01 21:52:50 -0800

    Add input validation and loop for number output

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main() {
-
-   /* Type your code here. */
+int num1, num2;
+cout <<"Enter 2 numbers, the second number must be bigger than the first.";
+cin >> num1 >> num2;
+if (num2 < num1)
+{
+   cout << "The second number must be bigger than the first.";
+}
+else 
+{
+   for (int i = num1; i <= num2; i+=5 )
+   {
+      cout << i << " ";
+   }
+}
 
    return 0;
 }
