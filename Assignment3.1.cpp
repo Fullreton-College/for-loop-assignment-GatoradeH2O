cb9d369ffeace39a66abbede9270488a3df58d54
LuisT <Gutytrejo1502@gmail.com>
2026-03-02 21:49:09 -0800

    Implement sales input and bar chart display
    Added functionality to input and display sales data as a bar chart.

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -5,6 +5,24 @@ using namespace std;
 
 int main()
 {
+	const int number_of_stores = 5;
+	double sales;
 
-return 0;
+	cout << "Sales bar chart"<<endl;
+	cout << "(Each * represents $100 in sales)"<<endl;
+
+	for (int store = 1; store <= number_of_stores; store++)
+	{
+		cout << "Enter today's sales for store #" << store << ": ";
+		cin >> sales;
+		cout << "Store #" << store << ": ";
+
+		for (int star = 0; star < sales / 100; star++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
